297ddb599d79dee9efd477a3b468e3ad5b43f929
Alanalezca <alana_forever@hotmail.com>
2025-06-12 09:59:33 +0200

    Build complet et mise à jour

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Articles from '../src/pages/articles/articles';
+import ArticlePage from '../src/pages/articles/articlePage';
 import Smashup from '../src/pages/smashup/smashup';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Articles />} />
+      <Route path="/article/:slug" element={<ArticlePage />} />
       <Route path="/smashup" element={<Smashup />} />
       <Route path="*" element={<Articles />} />
     </Routes>
