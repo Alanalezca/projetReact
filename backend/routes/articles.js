3254a23e90950df3e1b5657c4a9e1411de1b15df
Alanalezca <alana_forever@hotmail.com>
2025-09-05 00:47:47 +0200

    Build complet et mise à jour de la version 0.91

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     const result = await pool.query(`SELECT tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo" AS PseudoCreateur, tab_articles."LienImg", STRING_AGG(l_tags_articles."Libelle", ',') AS Tags FROM tab_affectations_tags_articles LEFT OUTER JOIN l_tags_articles ON tab_affectations_tags_articles."CodeTagArticle" = l_tags_articles."CodeTagArticle" RIGHT OUTER JOIN tab_articles INNER JOIN tab_users ON tab_articles."CreePar" = tab_users.id ON tab_affectations_tags_articles."CodeArticle" = tab_articles."CodeArticle" WHERE COALESCE(tab_articles."Publie", false) = true GROUP BY tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo", tab_articles."LienImg" ORDER BY "DateMaj" DESC`);
-    res.json(result.rows);
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error('Erreur lors de la récupération des articles:', err);
     res.status(500).json({ error: 'Erreur serveur' });
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 router.get('/all', checkSiCurrentUserGetGradeRequis('Administrateur'), async (req, res) => {
   try {
     const result = await pool.query(`SELECT tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo" AS PseudoCreateur, tab_articles."LienImg", STRING_AGG(l_tags_articles."Libelle", ',') AS Tags, tab_articles."Publie" AS "Publie" FROM tab_affectations_tags_articles LEFT OUTER JOIN l_tags_articles ON tab_affectations_tags_articles."CodeTagArticle" = l_tags_articles."CodeTagArticle" RIGHT OUTER JOIN tab_articles INNER JOIN tab_users ON tab_articles."CreePar" = tab_users.id ON tab_affectations_tags_articles."CodeArticle" = tab_articles."CodeArticle" GROUP BY tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo", tab_articles."LienImg" ORDER BY "DateCreation" DESC`);
-    res.json(result.rows);
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error('Erreur lors de la récupération des articles:', err);
     res.status(500).json({ error: 'Erreur serveur' });
@@ -28,11 +28,11 @@ router.get('/all', checkSiCurrentUserGetGradeRequis('Administrateur'), async (re
 router.get('/:slug', async (req, res) => {
   const { slug } = req.params;
   try {
-    const result = await pool.query(`SELECT tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo" AS "PseudoCreateur", tab_articles."LienImg", STRING_AGG(l_tags_articles."Libelle", ',') AS "Tags", tab_articles."Publie" AS "Publie" FROM tab_affectations_tags_articles LEFT OUTER JOIN l_tags_articles ON tab_affectations_tags_articles."CodeTagArticle" = l_tags_articles."CodeTagArticle" RIGHT OUTER JOIN (tab_articles INNER JOIN tab_users ON tab_articles."CreePar" = tab_users.id) ON tab_affectations_tags_articles."CodeArticle" = tab_articles."CodeArticle" WHERE tab_articles."Slug" = $1 GROUP BY tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo", tab_articles."LienImg"`, [slug]);
-    res.json(result.rows);
+    const result = await pool.query(`SELECT tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo" AS "PseudoCreateur", tab_articles."LienImg", STRING_AGG(l_tags_articles."Libelle", ',') AS "Tags", tab_articles."Publie" AS "Publie", tab_articles."NbVues" +1 AS "NbVues" FROM tab_affectations_tags_articles LEFT OUTER JOIN l_tags_articles ON tab_affectations_tags_articles."CodeTagArticle" = l_tags_articles."CodeTagArticle" RIGHT OUTER JOIN (tab_articles INNER JOIN tab_users ON tab_articles."CreePar" = tab_users.id) ON tab_affectations_tags_articles."CodeArticle" = tab_articles."CodeArticle" WHERE tab_articles."Slug" = $1 GROUP BY tab_articles."CodeArticle", tab_articles."Titre", tab_articles."Resume", tab_articles."Slug", tab_articles."Contenu", tab_articles."DateCreation", tab_articles."DateMaj", tab_articles."CreePar", tab_users."pseudo", tab_articles."LienImg", tab_articles."NbVues"`, [slug]);
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'Article non trouvé' });
     }
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error(`Erreur lors de la récupération de l'articles:`, err);
     res.status(500).json({ error: 'Erreur serveur' });
@@ -128,9 +128,24 @@ router.post('/delete', checkSiCurrentUserGetGradeRequis('Administrateur'), async
       return res.status(500).json({ error: "Échec de la suppression de l'article" });
     }
 
-    res.status(201).json(result.rows[0]);
+    res.status(204).json(result.rows[0]);
   } catch (err) {
     console.error(`Erreur lors de la suppression de l'article :`, err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+
+router.post('/incrementViewArticle', async (req, res) => {
+  const { parCodeArticle } = req.body;
+  
+  try {
+    const result = await pool.query(`UPDATE tab_articles SET "NbVues" = "NbVues" +1 WHERE "CodeArticle" = $1 RETURNING *`,
+      [parCodeArticle]);
+
+    res.status(204).json(result.rows[0]);
+  } catch (err) {
+    console.error(`Échec de l'incrémentation du nombre de vues' :`, err);
     res.status(500).json({ error: 'Erreur serveur' });
   }
 });
