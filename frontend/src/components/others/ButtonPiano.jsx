c9a68414ba634a4c3553c498b3aac72af3930049
Alanalezca <alana_forever@hotmail.com>
2025-10-06 01:15:05 +0200

    Build complet et mise à jour

--- frontend/src/components/others/ButtonPiano.jsx
+++ frontend/src/components/others/ButtonPiano.jsx
@@ -6,7 +6,7 @@ const ButtonPiano = ({arrayLibelleOccurences, currentOccurenceInFocus, setterCur
     return (
         <div className={`btn-group ${styles.piano}`} role="group" aria-label="Basic outlined example">
             {arrayLibelleOccurences.map((currentOccurence, index) => (
-                <button key={`btn-${index}`} type="button" className={`btn btn-outline-primary ${currentOccurenceInFocus == index && styles.selected}`} onClick={() => setterCurrentOccurenceInFocus(index)}>{currentOccurence}</button>
+                <button key={`btn-${index}`} type="button" className={`btn btn-outline-primary ${currentOccurenceInFocus == index && styles.selected}`} onClick={() => (setterCurrentOccurenceInFocus && setterCurrentOccurenceInFocus(index))}>{currentOccurence}</button>
             ))}
         </div>
     );
