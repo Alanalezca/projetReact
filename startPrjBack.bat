5c1296fb0c36417cda5fe3fd9b381e6c0b91d1f8
Alanalezca <alana_forever@hotmail.com>
2025-11-26 23:08:40 +0100

    Remove ignored files from repository

--- startPrjBack.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Démarrer le backend Express
-cd /d C:\Users\alana\OneDrive\NexusRE\backend
-start cmd /k "npm start"
--- startPrjFront.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Démarrer le frontend React
-cd /d C:\Users\alana\OneDrive\NexusRE\frontend
-start cmd /k "npm start"
